1706207fbcda44a168746bfb9ee69a0236be3531
Kai Hendry <hendry@iki.fi>
2020-12-09 14:34:33 +0800

    Refactored to just GET/POST

--- Makefile
+++ Makefile
@@ -1,7 +1,15 @@
+STACK = aws-sam-gateway-example
+
 .PHONY: build deploy validate destroy
 
 deploy: build
-	AWS_PROFILE=mine sam deploy
+	AWS_PROFILE=mine sam deploy --stack-name $(STACK)
 
 build:
 	CGO_ENABLED=0 sam build
+
+validate:
+	aws cloudformation validate-template --template-body file://template.yaml
+
+destroy:
+	AWS_PROFILE=mine aws cloudformation delete-stack --stack-name $(STACK)
--- handlers.go
+++ handlers.go
@@ -2,54 +2,26 @@ package awssamgatewayexample
 
 import (
 	"fmt"
-	"io/ioutil"
 	"net/http"
+	"time"
 
 	"github.com/apex/log"
 	"github.com/gorilla/pat"
 )
 
-// pets database-ish
-var pets = make(map[string]struct{})
-
 func App() *pat.Router {
 	app := pat.New()
 	app.Get("/", get)
 	app.Post("/", post)
-	app.Post("/healthz", healthHandler)
 	return app
 }
 
-func healthHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("ok"))
-}
-
 func get(w http.ResponseWriter, r *http.Request) {
-	log.Info("list pets")
-
-	if len(pets) == 0 {
-		fmt.Fprintf(w, "no pets")
-		return
-	}
-
-	for name := range pets {
-		fmt.Fprintf(w, "- %s\n", name)
-	}
+	log.Info("saying hello at current time")
+	fmt.Fprintf(w, "GET response: hello at %s!\n", time.Now())
 }
 
-// curl -d Tobi http://localhost:3000/
-// curl -d Loki http://localhost:3000/
-// curl -d Jane http://localhost:3000/
 func post(w http.ResponseWriter, r *http.Request) {
-	b, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
-		return
-	}
-
-	name := string(b)
-	pets[name] = struct{}{}
-	log.WithField("name", name).Info("add pet")
-	fmt.Fprintf(w, "welcome to the family %s!\n", name)
+	log.Info("going Postal")
+	fmt.Fprintf(w, "POST response: hello at %s!\n", time.Now())
 }
